6c6101f21ae190468f1d1f24d38268b1c6f5467c
vhuhwavhohau <mavhandu3@ymail.com>
2018-08-01 14:34:54 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  on:boolean = false;
+  off:boolean = false;
+  constructor(public navCtrl: NavController,private flashlight: Flashlight) {
+  this.on = true;
   }
+  
+  
+on1(){
+  this.flashlight.switchOn()
+  this.on = false;
+  this.off = true;
+}
+off1(){
+  this.flashlight.switchOff()
+  this.on = true;
+  this.off = false;
+}
+
 
 }
